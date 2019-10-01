a26037f490b3da2066c1518af59332f00aa22248
thedestroyer00 <53038045+thedestroyer00@users.noreply.github.com>
2019-10-01 00:40:50 -0700

    Update beautifulsoup_and_requests.py

--- beautifulsoup_and_requests.py
+++ beautifulsoup_and_requests.py
@@ -1,6 +1,15 @@
 from bs4 import BeautifulSoup
 import requests 
 
-url = 'https://github.com'
+url = 'https://coreyms.com'
 response = requests.get(url).text
+
+soup = BeautifulSoup(response, 'lxml')
+
+article = soup.find('article')
+
+link = article.find(class_ = 'entry-title-link')['href']
+
+print(link)
+
 
