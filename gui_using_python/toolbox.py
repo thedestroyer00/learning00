ff946ac3f1fb128af23499293bc50a2d8e7920ba
thedestroyer00 <53038045+thedestroyer00@users.noreply.github.com>
2020-01-29 19:18:24 +0545

    Create toolbox.py

---
+++ gui_using_python/toolbox.py
@@ -0,0 +1,27 @@
+from PyQt5.QtWidgets import *
+import sys
+
+class Window(QWidget):
+
+    def __init__(self):
+        QWidget.__init__(self)
+        layout = QGridLayout()
+        self.setLayout(layout)
+
+        # Add toolbar and items
+        toolbox = QToolBox()
+        layout.addWidget(toolbox)
+        label = QLabel()
+        toolbox.addItem(label, "Students")
+        label = QLabel()
+        toolbox.addItem(label, "Teachers")
+        label = QLabel()
+        toolbox.addItem(label, "Directors")
+
+        textfield = QPlainTextEdit()
+        layout.addWidget(textfield)
+
+app = QApplication(sys.argv)
+screen = Window()
+screen.show()
+sys.exit(app.exec_())
