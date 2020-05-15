8f83b612d48867463162f0f6bc46ea3634cf0654
thedestroyer00 <53038045+thedestroyer00@users.noreply.github.com>
2020-05-14 18:12:54 -0900

    Create 2.py

---
+++ coding_challenges/2.py
@@ -0,0 +1,23 @@
+#Problem: 
+#   Given an array of integers, return a new array such that each element at index i of 
+#   the new array is the product of all the numbers in the original array except the one at i.
+#   For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24].
+#   If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+
+#Solution: 
+import copy
+def findprod(nums):
+	prod = []
+	for i in nums:
+		dumlist = copy.deepcopy(nums)
+		dumlist.remove(i)
+		p = 1
+		for j in dumlist:
+			p*=j
+		prod.append(p)
+	print(prod)
+
+
+nums = [3,2,1]
+findprod(nums)
